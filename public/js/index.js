05599fc9fec3b81ce18a8394500db29bdac9c782
Antti Sand <antti.sand@gmail.com>
2019-01-30 11:09:45 +0200

    Move js to separate file

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+let socket = io();
+socket.on('connect', () => {
+    console.log("Connected to server")
+
+    socket.emit('createMessage', {
+        "from": "Antti",
+        "text": "Nope"
+    })
+})
+socket.on('disconnect', () => {
+    console.log("Disconnected from server")
+})
+
+socket.on('newMessage', (message) => {
+    console.log("New message", message)
+})
