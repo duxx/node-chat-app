d9c51e604d6f87ab905fddecc4ae2d9d3d3b2f15
Antti Sand <antti.sand@gmail.com>
2019-01-30 13:53:07 +0200

    Add support for users list and rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,24 +2,64 @@ let form = document.getElementById("message-form")
 let input = document.querySelector('[name=message]')
 let messages = document.getElementById("messages")
 let locationButton = document.getElementById('send-location')
+let usersDiv = document.getElementById('users')
+let template = document.getElementById("message-template").innerHTML
+let locationTemplate = document.getElementById("location-message-template").innerHTML
+
+let scrollToBottom = () => {
+    let clientHeight = messages.clientHeight
+    let scrollTop = messages.scrollTop
+    let scrollHeight = messages.scrollHeight
+    let newMessage = messages.children.querySelector('div.message-wrap:last-child')
+    
+}
 
 let socket = io();
 socket.on('connect', () => {
-    console.log("Connected to server")
+    let params = deparam()
+    socket.emit('join', params, (err) => {
+        if(err) {
+            alert(err)
+            window.location.href = "/"
+        } else {
+            console.log("Data ok")
+        }
+    })
 })
 
 socket.on('disconnect', () => {
     console.log("Disconnected from server")
 })
 
+socket.on('updateUsersList', (users) => {
+    let ol = document.createElement('ol')
+    users.forEach((user) => {
+        let node = document.createElement('li')
+        node.innerText = user
+        ol.appendChild(node)
+    })
+    usersDiv.innerHTML = ol.outerHTML
+    console.log(users)
+})
+
 socket.on('newMessage', (message) => {
     let time = moment(message.createdAt).format('hh:mm')
-    messages.innerHTML += `[${time}] ${message.from}: ${message.text}<br/>`
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: time
+    })
+    messages.innerHTML += html
 })
 
 socket.on('newLocationMessage', (message) => {
     let time = moment(message.createdAt).format('hh:mm')
-    messages.innerHTML += `[${time}] ${message.from}: <a href="${message.url}" target="_blank">View my location</a><br/>`
+    let html = Mustache.render(locationTemplate, {
+        url: message.url,
+        from: message.from,
+        createdAt: time
+    })
+    messages.innerHTML += html
 })
 
 form.addEventListener('submit', (e) => {
