5834cfb14ff8e9a0809e04bd6af692ff76d481d8
Antti Sand <antti.sand@gmail.com>
2019-01-30 11:58:08 +0200

    Add ability to share location

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,13 @@
 let form = document.getElementById("message-form")
 let input = document.querySelector('[name=message]')
 let messages = document.getElementById("messages")
+let locationButton = document.getElementById('send-location')
 
 let socket = io();
 socket.on('connect', () => {
     console.log("Connected to server")
-
-    socket.emit('createMessage', {
-        "from": "Antti",
-        "text": "Nope"
-    })
 })
+
 socket.on('disconnect', () => {
     console.log("Disconnected from server")
 })
@@ -20,6 +17,10 @@ socket.on('newMessage', (message) => {
     messages.innerHTML += `${message.from}: ${message.text}<br/>`
 })
 
+socket.on('newLocationMessage', (message) => {
+    messages.innerHTML += `${message.from}: <a href="${message.url}" target="_blank">View my location</a><br/>`
+})
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     socket.emit('createMessage', {
@@ -27,4 +28,20 @@ form.addEventListener('submit', (e) => {
         text: input.value
     })
     input.value = ''
+})
+
+locationButton.addEventListener('click', (e) => {
+    if(!navigator.geolocation) {
+        return alert("Geolocation is not supported by your browser.")
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, (e) => {
+        alert('Unable to geolocate')
+    })
 })
