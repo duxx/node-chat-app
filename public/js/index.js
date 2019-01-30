52155ee2fdb6bc928f24378a8bd666f6a7a8a3e8
Antti Sand <antti.sand@gmail.com>
2019-01-30 11:40:04 +0200

    UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,7 @@
+let form = document.getElementById("message-form")
+let input = document.querySelector('[name=message]')
+let messages = document.getElementById("messages")
+
 let socket = io();
 socket.on('connect', () => {
     console.log("Connected to server")
@@ -13,4 +17,14 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
     console.log("New message", message)
+    messages.innerHTML += `${message.from}: ${message.text}<br/>`
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    socket.emit('createMessage', {
+        from: 'User',
+        text: input.value
+    })
+    input.value = ''
 })
