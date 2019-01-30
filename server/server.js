5082ee96d5c8bf71b15efcc79c183b6b159da426
Antti Sand <antti.sand@gmail.com>
2019-01-30 11:24:12 +0200

    Use message generator function

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
+const {generateMessage} = require('./utils/message')
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
@@ -14,14 +15,13 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log("New user connected")
 
-    socket.emit('newMessage', {
-        from: "User 1",
-        text: "Hello!",
-        createdAt: new Date().toDateString()
-    })
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app!'))
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
     socket.on('createMessage', (message) => {
         console.log("createMessage", message)
+        io.emit('newMessage', generateMessage(message.from, message.text))
     })
 
     socket.on('disconnect', (socket) => {
